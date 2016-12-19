b72dc549ab796cb28a6e50a812bee734e2a286b0
Robin Wagner <robinwagner@Robins-MBP.localdomain>
2016-12-19 20:57:44 +0100

    Favorite number exercise completed

---
+++ Chapter5/favorite_number.rb
@@ -0,0 +1,5 @@
+# Bigger, better favorite number
+
+puts "What is your favorite number?"
+number = gets.chomp.to_i
+puts "Great! But your know what is an even better favorite number? #{number + 1}!"
