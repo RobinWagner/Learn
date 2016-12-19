8e3f7c33654307e6a2380830794c5f10bb89dd3b
Robin Wagner <robinwagner@Robins-MBP.localdomain>
2016-12-19 07:51:00 +0100

    Exercises chapter 2 completed

---
+++ Chapter1/chapter2.rb
@@ -0,0 +1,15 @@
+# Hours in a year
+
+puts "Hours in a year: #{365.25 * 24}"
+
+# Minutes in a decade
+
+puts "Minutes in a decade: #{10 * 365.25 * 24 * 60}"
+
+# Age in seconds
+
+puts "Age in seconds: #{28 * 365.25 * 24 * 60 * 60}"
+
+# Author's age
+
+puts "Author's age: #{(1025 * 1_000_000) / 60 / 60 / 24 / 365.25}"
