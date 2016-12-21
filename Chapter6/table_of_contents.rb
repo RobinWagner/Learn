8df1d1e11db4b9b0b3360fafb496fa3d3bf816c0
Robin Wagner <robin-wagner@web.de>
2016-12-21 20:31:51 +0100

    Table of contents exercise completed

---
+++ Chapter6/table_of_contents.rb
@@ -0,0 +1,8 @@
+# Table of contents
+
+line_width = 50
+puts "Table of Contents".center(line_width)
+puts ""
+puts "Chapter1:  Getting Started".ljust(line_width/2) + "page  1".rjust(line_width/2)
+puts "Chapter2:  Numbers".ljust(line_width/2) + "page  9".rjust(line_width/2)
+puts "Chapter3:  Letters".ljust(line_width/2) + "page 13".rjust(line_width/2)
