213dc22d84eb52fa313f8651041b116930392531
Robin Wagner <robin-wagner@web.de>
2016-12-22 16:10:30 +0100

    Exercise 99 bottles of beer completed.

---
+++ Chapter7/99_bottles.rb
@@ -0,0 +1,13 @@
+# 99 bottles of beer on the wall
+
+num_at_start= 99
+
+num_bot = proc { |n| "#{n} bottle#{ n == 1 ? '' : 's' }" }
+
+num_at_start.downto(2) do |num|
+  puts "#{num_bot[num]} of beer on the wall, #{num_bot[num]} of beer!"
+  puts "Take one down, pass it around, #{num_bot[num-1]} of beer on the wall!"
+end
+
+puts "#{num_bot[1]} of beer on the wall, #{num_bot[1]} of beer!"
+puts "Take one down, pass it around, no more bottles of beer on the wall!"
